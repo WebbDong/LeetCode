23dde1a2505e37d33bd6ad5c6ea5bedb3433f4d0
WebbDong <dwbdeveloper1991@163.com>
2020-12-03 13:21:44 +0800

    ReverseLinkedList_206

---
+++ src/main/java/com/dong/leetcode/simple/ReverseLinkedList_206.java
@@ -0,0 +1,70 @@
+package com.dong.leetcode.simple;
+
+/**
+ * 反转一个单链表。
+ *      输入: 1->2->3->4->5->NULL
+ *      输出: 5->4->3->2->1->NULL
+ *
+ * 进阶:
+ *      你可以迭代或递归地反转链表。你能否用两种方法解决这道题？
+ *
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class ReverseLinkedList_206 {
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) { val = x; }
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        ListNode node1 = new ListNode(2);
+        ListNode node2 = new ListNode(3);
+        ListNode node3 = new ListNode(4);
+        ListNode node4 = new ListNode(5);
+        head.next = node1;
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+        printLinkedList(head);
+        printLinkedList(reverseList(head));
+    }
+
+    public static ListNode reverseList(ListNode head) {
+        // 1->2->3->4->5->NULL
+        // 遍历的当前节点
+        ListNode current = head;
+        // 遍历的下一个节点
+        ListNode next;
+        // 新头节点
+        ListNode newHead = null;
+        while (current != null) {
+            next = current.next;
+            current.next = newHead;
+            newHead = current;
+            current = next;
+        }
+        return newHead;
+    }
+
+    public static void printLinkedList(ListNode head) {
+        if (head == null) {
+            return;
+        }
+
+        ListNode node = head;
+        do {
+            System.out.print(node.val + " ");
+            node = node.next;
+        } while (node != null);
+        System.out.println();
+    }
+
+}
