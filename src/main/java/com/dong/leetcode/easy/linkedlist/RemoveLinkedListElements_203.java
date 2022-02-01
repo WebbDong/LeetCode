0c1aa82cdca68377c0fe89cff57277e11fd00a22
webbdong <wangwebb1991@gmail.com>
2022-02-01 23:06:50 +0800

    203

---
+++ src/main/java/com/dong/leetcode/easy/linkedlist/RemoveLinkedListElements_203.java
@@ -0,0 +1,63 @@
+package com.dong.leetcode.easy.linkedlist;
+
+/**
+ * 给你一个链表的头节点 head 和一个整数 val ，请你删除链表中所有满足 Node.val == val 的节点，并返回 新的头节点 。
+ *
+ * 输入：head = [1,2,6,3,4,5,6], val = 6
+ * 输出：[1,2,3,4,5]
+ *
+ * 输入：head = [], val = 1
+ * 输出：[]
+ *
+ * 输入：head = [7,7,7,7], val = 7
+ * 输出：[]
+ *
+ * 提示：
+ *      列表中的节点数目在范围 [0, 104] 内
+ *      1 <= Node.val <= 50
+ *      0 <= val <= 50
+ */
+public class RemoveLinkedListElements_203 {
+
+    private static class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) {
+            this.val = val;
+        }
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    private static ListNode removeElements(ListNode head, int val) {
+        return null;
+    }
+
+    private static void printAll(ListNode head) {
+        while (head != null) {
+            System.out.println(head.val);
+            head = head.next;
+        }
+    }
+
+    public static void main(String[] args) {
+        ListNode node1 = new ListNode(1);
+        ListNode node2 = new ListNode(2);
+        ListNode node3 = new ListNode(6);
+        ListNode node4 = new ListNode(3);
+        ListNode node5 = new ListNode(4);
+        ListNode node6 = new ListNode(5);
+        ListNode node7 = new ListNode(6);
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+        node4.next = node5;
+        node5.next = node6;
+        node6.next = node7;
+        printAll(node1);
+    }
+
+}
