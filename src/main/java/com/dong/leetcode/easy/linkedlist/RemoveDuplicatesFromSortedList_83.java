708b84a7c53b95c57190c312a971903069f1cfb5
webbdong <wangwebb1991@gmail.com>
2022-02-02 13:29:03 +0800

    83

--- src/main/java/com/dong/leetcode/easy/linkedlist/RemoveDuplicatesFromSortedList_83.java
+++ src/main/java/com/dong/leetcode/easy/linkedlist/RemoveDuplicatesFromSortedList_83.java
@@ -1,6 +1,57 @@
 package com.dong.leetcode.easy.linkedlist;
 
 /**
+ * 给定一个已排序的链表的头 head ， 删除所有重复的元素，使每个元素只出现一次 。返回 已排序的链表 。
+ *
+ * 输入：head = [1,1,2]
+ * 输出：[1,2]
+ *
+ * 输入：head = [1,1,2,3,3]
+ * 输出：[1,2,3]
+ *
+ * 提示：
+ *      链表中节点数目在范围 [0, 300] 内
+ *      -100 <= Node.val <= 100
+ *      题目数据保证链表已经按升序 排列
  */
 public class RemoveDuplicatesFromSortedList_83 {
+
+    private static class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) {
+            this.val = val;
+        }
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    private static ListNode deleteDuplicates(ListNode head) {
+        return null;
+    }
+
+    private static void printAll(ListNode head) {
+        while (head != null) {
+            System.out.print(head.val + ", ");
+            head = head.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        ListNode node1 = new ListNode(1);
+        ListNode node2 = new ListNode(1);
+        ListNode node3 = new ListNode(2);
+        ListNode node4 = new ListNode(3);
+        ListNode node5 = new ListNode(3);
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+        node4.next = node5;
+        printAll(node1);
+    }
+
 }
