248c40b8d809eccd007e7ff83c529eed3c676309
webbdong <wangwebb1991@gmail.com>
2022-02-03 01:18:35 +0800

    82

--- src/main/java/com/dong/leetcode/medium/linkedlist/RemoveDuplicatesFromSortedListII_82.java
+++ src/main/java/com/dong/leetcode/medium/linkedlist/RemoveDuplicatesFromSortedListII_82.java
@@ -16,7 +16,49 @@ package com.dong.leetcode.medium.linkedlist;
  */
 public class RemoveDuplicatesFromSortedListII_82 {
 
+    private static class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) {
+            this.val = val;
+        }
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    private static ListNode deleteDuplicates(ListNode head) {
+
+        return null;
+    }
+
+    private static void printAll(ListNode head) {
+        while (head != null) {
+            System.out.print(head.val + ", ");
+            head = head.next;
+        }
+        System.out.println();
+    }
+
     public static void main(String[] args) {
+        ListNode node1 = new ListNode(1);
+        ListNode node2 = new ListNode(2);
+        ListNode node3 = new ListNode(3);
+        ListNode node4 = new ListNode(3);
+        ListNode node5 = new ListNode(4);
+        ListNode node6 = new ListNode(4);
+        ListNode node7 = new ListNode(5);
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+        node4.next = node5;
+        node5.next = node6;
+        node6.next = node7;
+        printAll(node1);
+        deleteDuplicates(node1);
+        printAll(node1);
     }
 
 }
