b7ae3995e20b6d63cc7381e759571d3210c90640
webbdong <wangwebb1991@gmail.com>
2022-02-05 22:07:26 +0800

    876

---
+++ src/main/java/com/dong/leetcode/easy/linkedlist/MiddleOfTheLinkedList_876.java
@@ -0,0 +1,70 @@
+package com.dong.leetcode.easy.linkedlist;
+
+/**
+ * 给定一个头结点为 head 的非空单链表，返回链表的中间结点。
+ * 如果有两个中间结点，则返回第二个中间结点。
+ *
+ * 输入：[1,2,3,4,5]
+ * 输出：此列表中的结点 3 (序列化形式：[3,4,5])
+ * 返回的结点值为 3 。 (测评系统对该结点序列化表述是 [3,4,5])。
+ * 注意，我们返回了一个 ListNode 类型的对象 ans，这样：
+ * ans.val = 3, ans.next.val = 4, ans.next.next.val = 5, 以及 ans.next.next.next = NULL.
+ *
+ * 输入：[1,2,3,4,5,6]
+ * 输出：此列表中的结点 4 (序列化形式：[4,5,6])
+ * 由于该列表有两个中间结点，值分别为 3 和 4，我们返回第二个结点。
+ *
+ * 提示：
+ *      给定链表的结点数介于 1 和 100 之间。
+ */
+public class MiddleOfTheLinkedList_876 {
+
+    private static class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) {
+            this.val = val;
+        }
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    private static ListNode middleNode(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+
+        int size = 0;
+        ListNode temp = head;
+        while (temp != null) {
+            size++;
+            temp = temp.next;
+        }
+        int middle = size / 2;
+        temp = head;
+        for (int i = 0; i < middle; i++) {
+            temp = temp.next;
+        }
+        return temp;
+    }
+
+    public static void main(String[] args) {
+        ListNode node1 = new ListNode(1);
+        ListNode node2 = new ListNode(2);
+        ListNode node3 = new ListNode(3);
+        ListNode node4 = new ListNode(4);
+        ListNode node5 = new ListNode(5);
+        ListNode node6 = new ListNode(6);
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+        node4.next = node5;
+        node5.next = node6;
+        ListNode middleNode = middleNode(node1);
+        System.out.println(middleNode.val);
+    }
+
+}
