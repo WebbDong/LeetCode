3ae10051efcc0f95b9dd588fe101e30e9896788d
webbdong <wangwebb1991@gmail.com>
2022-02-06 20:18:45 +0800

    141

--- src/main/java/com/dong/leetcode/easy/linkedlist/LinkedListCycle_141.java
+++ src/main/java/com/dong/leetcode/easy/linkedlist/LinkedListCycle_141.java
@@ -39,11 +39,10 @@ public class LinkedListCycle_141 {
     /**
      * 快慢指针
      */
-    public static boolean hasCycle(ListNode head) {
+    private static boolean hasCycle(ListNode head) {
         if (head == null || head.next == null) {
             return false;
         }
-
         ListNode slow = head;
         ListNode fast = head.next;
         while (fast != null && fast.next != null) {
@@ -56,6 +55,25 @@ public class LinkedListCycle_141 {
         return false;
     }
 
+    /**
+     * 快慢指针2
+     */
+    private static boolean hasCycle2(ListNode head) {
+        if (head == null || head.next == null) {
+            return false;
+        }
+        ListNode slow = head;
+        ListNode fast = head.next;
+        while (slow != fast) {
+            if (fast == null || fast.next == null) {
+                return false;
+            }
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return true;
+    }
+
     public static void main(String[] args) {
         ListNode node1 = new ListNode(3);
         ListNode node2 = new ListNode(2);
@@ -66,6 +84,7 @@ public class LinkedListCycle_141 {
         node3.next = node4;
         node4.next = node2;
         System.out.println(hasCycle(node1));
+        System.out.println(hasCycle2(node1));
     }
 
 }
